e22970f835b4199361de8365a67c0df8e0769ff5
Navaneethan <navaneethan.ramasamy@turing.com>
2022-09-18 18:54:34 +0530

    initial json data and bmi module implementation

---
+++ bmi.py
@@ -0,0 +1,58 @@
+import json
+import argparse
+import os
+
+
+def bmi_calculation(row_dict):
+    '''
+    row_dict: @type: dict
+    @desc: contains WeightKg and HeightCm for bml calc
+    @returns: adding 3 new fields bmi_cat, bmi_range, health_risk
+    '''
+    bmi_value = (row_dict['WeightKg'] / ((row_dict['HeightCm'] / 100.0) ** 2))
+    if bmi_value <= 18.4:
+        row_dict['BmiCategory'] = 'Underweight'
+        row_dict['BmiRange'] = '18.4 and below'
+        row_dict['HealthRisk'] = 'Malnutrition risk'
+    elif bmi_value >= 18.5 and bmi_value <= 24.9:
+        row_dict['BmiCategory'] = 'Normal weight'
+        row_dict['BmiRange'] = '18.5 and 24.9'
+        row_dict['HealthRisk'] = 'Low risk'
+    elif bmi_value >= 25 and bmi_value <= 29.9:
+        row_dict['BmiCategory'] = 'Overweight'
+        row_dict['BmiRange'] = '25 and 29.9'
+        row_dict['HealthRisk'] = 'Enhanced risk'
+    elif bmi_value >= 30 and bmi_value <= 34.9:
+        row_dict['BmiCategory'] = 'Moderately obese'
+        row_dict['BmiRange'] = '30 and 34.9'
+        row_dict['HealthRisk'] = 'Medium risk'
+    elif bmi_value >= 35 and bmi_value <= 39.9:
+        row_dict['BmiCategory'] = 'Severely obese'
+        row_dict['BmiRange'] = '35 and 39.9'
+        row_dict['HealthRisk'] = 'High risk'
+    elif bmi_value >= 40:
+        row_dict['BmiCategory'] = 'Very severely obese'
+        row_dict['BmiRange'] = '40 and above'
+        row_dict['HealthRisk'] = 'Very High risk'
+        
+    print(row_dict)
+
+def run(data):
+    '''
+    data @type: list of dict
+    @return : dict with additional columns
+    '''
+    for row in data:
+        bmi_calculation(row)
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--jsonFile', action='store', required=True)
+    args = arg_parser.parse_args()
+    json_file_path = args.jsonFile
+    print(json_file_path)
+    if os.path.exists(json_file_path):
+        with open(json_file_path) as json_file:
+            bmi_data = json.loads(json_file.read())
+            print(bmi_data)
+            run(bmi_data)
